1d8672ddf365edad8c242fc74cfd77e8c5614838
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-13 22:55:25 -0500

    updated code, might start it over

--- small_town_teller.py
+++ small_town_teller.py
@@ -13,62 +13,59 @@ class Account:
         self.balance = balance
 
 
-class Bank():
-    balance = 0
+class Bank(Account, Person):
+    def __init__(self):
+        self.customer = {}
 
-    def add_account(self, customer):
-
-        return customer
-
-    def deposit(self, num, money):
-        if num == num:
-            self.balance += money
+    def add_customer(self, customer, account):
+        if customer not in self.customer:
+            self.customer.update({customer: account})
         else:
-            print("Sorry, your account number was not recognized")
+            print("Customer already exists")
 
-    def withdrawal(self, num, money):
-        if num == num:
-            self.balance -= money
-        else:
-            print("Sorry, the account number you entered is not correct")
+    def add_account(self, customer, account):
+        if customer in self.customer:
+            self.customer[customer] += account
 
-    def balance_inquiry(self, num):
-        if num == num:
-            return self.balance
-        else:
-            print("Sorry. that was the wrong account number.")
+
+
+dict1 = {"Test": ["a string", "another string"]}
+
+dict1["Test"] += ["and another string", "one more string"]
+
+print(dict1)
+
 
 
 zc_bank = Bank()
 bob = Person(1, "Bob", "Smith")
-zc_bank.add_account(bob)
+bob_checking = Account(2001, "CHECKING", bob)
+zc_bank.add_customer(bob, bob_checking)
 bob_savings = Account(1001, "SAVINGS", bob)
-zc_bank.add_account(bob_savings)
-zc_bank.balance_inquiry(1001)
-# 0
-zc_bank.deposit(1001, 256.02)
-zc_bank.balance_inquiry(1001)
-# 256.02
-zc_bank.withdrawal(1001, 128)
-(zc_bank.balance_inquiry(1001))
-print(zc_bank.balance_inquiry(1001))
-# 128.02
-
-wsfs_bank = Bank()
-imir = Person(7, "Imir", "Ransom")
-wsfs_bank.add_account(imir)
-imir_checking = Account(7707, "CHECKING", imir)
-wsfs_bank.add_account(imir_checking)
-wsfs_bank.balance_inquiry(7707)
-# 0
-wsfs_bank.deposit(7707, 1300)
-wsfs_bank.balance_inquiry(7077)
-# 1300
-wsfs_bank.withdrawal(7707, 200)
-wsfs_bank.balance_inquiry(7707)
-print(wsfs_bank.balance_inquiry(7707))
-
-
+zc_bank.add_account(bob, bob_savings)
+# zc_bank.balance_inquiry(1001)
+# # 0
+# zc_bank.deposit(1001, 256.02)
+# zc_bank.balance_inquiry(1001)
+# # 256.02
+# zc_bank.withdrawal(1001, 128)
+# (zc_bank.balance_inquiry(1001))
+# print(zc_bank.balance_inquiry(1001))
+# # 128.02
+#
+# wsfs_bank = Bank()
+# imir = Person(7, "Imir", "Ransom")
+# wsfs_bank.add_account(imir)
+# imir_checking = Account(7707, "CHECKING", imir)
+# wsfs_bank.add_account(imir_checking)
+# wsfs_bank.balance_inquiry(7707)
+# # 0
+# wsfs_bank.deposit(7707, 1300)
+# wsfs_bank.balance_inquiry(7077)
+# # 1300
+# wsfs_bank.withdrawal(7707, 200)
+# wsfs_bank.balance_inquiry(7707)
+# print(wsfs_bank.balance_inquiry(7707))
 
 
 
