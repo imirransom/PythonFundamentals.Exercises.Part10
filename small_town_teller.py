9deb6104cc4f20172495d8f7377309bcdcaeb2ef
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-13 19:45:35 -0500

    program works, but not how expected

--- small_town_teller.py
+++ small_town_teller.py
@@ -4,33 +4,36 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
 
+
 class Account:
-    def __init__(self, number=1001, account_type="CHECKING", owner=Person(7, "", ""), balance=0):
+    def __init__(self, number, account_type, owner, balance=0):
         self.number = number
         self.account_type = account_type
         self.owner = owner
         self.balance = balance
 
 
-class Bank(Account):
+class Bank():
+    balance = 0
+
     def add_account(self, customer):
-        if customer == (self.number, self.account_type, self.owner):
-            return self.owner
+
+        return customer
 
     def deposit(self, num, money):
-        if num == self.number:
+        if num == num:
             self.balance += money
         else:
             print("Sorry, your account number was not recognized")
 
     def withdrawal(self, num, money):
-        if num == self.number:
+        if num == num:
             self.balance -= money
         else:
             print("Sorry, the account number you entered is not correct")
 
     def balance_inquiry(self, num):
-        if num == self.number:
+        if num == num:
             return self.balance
         else:
             print("Sorry. that was the wrong account number.")
@@ -48,8 +51,22 @@ zc_bank.balance_inquiry(1001)
 # 256.02
 zc_bank.withdrawal(1001, 128)
 (zc_bank.balance_inquiry(1001))
+print(zc_bank.balance_inquiry(1001))
 # 128.02
 
+wsfs_bank = Bank()
+imir = Person(7, "Imir", "Ransom")
+wsfs_bank.add_account(imir)
+imir_checking = Account(7707, "CHECKING", imir)
+wsfs_bank.add_account(imir_checking)
+wsfs_bank.balance_inquiry(7707)
+# 0
+wsfs_bank.deposit(7707, 1300)
+wsfs_bank.balance_inquiry(7077)
+# 1300
+wsfs_bank.withdrawal(7707, 200)
+wsfs_bank.balance_inquiry(7707)
+print(wsfs_bank.balance_inquiry(7707))
 
 
 
