e8a715d41cae247f855af785d57f76cfb020578f
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-14 13:28:33 -0500

    updated some code

--- small_town_teller.py
+++ small_town_teller.py
@@ -13,45 +13,93 @@ class Account:
         self.balance = balance
 
 
-class Bank(Account, Person):
+class Bank():
     def __init__(self):
-        self.customer = {}
+        self.customer = []
+        self.account = []
 
-    def add_customer(self, customer, account):
-        if customer not in self.customer:
-            self.customer.update({customer: account})
-        else:
-            print("Customer already exists")
+    def add_customer(self, customer):
+        number = False
+        for item in self.customer:
+            if item.id == customer:
+                number = False
+                print("ID number already exists")
+            else:
+                if not number:
+                    self.customer.append(customer)
 
-    def add_account(self, customer, account):
-        if customer in self.customer:
-            self.customer[customer] += account
+    def add_account(self, customer):
+        number = False
+        for item in self.account:
+            if item.id == customer:
+                number = False
+                print("ID number already exists")
+            else:
+                if not number:
+                    self.account.append(customer)
+
+
+    def deposit(self, account, num):
+        for item in self.account:
+            if item.number == account:
+                item.balance += num
+
+
+    def withdrawal(self, account, num):
+        for item in self.account:
+            if item.number == account:
+                item.balance -= num
+
+
+    def balance_inquiry(self, account):
+        for item in self.account:
+            if item.number == account:
+                return item.balance
+
+    def remove_account(self, account):
+        for item in account:
+            if item.number == account:
+                self.account.remove(account)
 
 
 
-dict1 = {"Test": ["a string", "another string"]}
+            # if customer not in self.customer:
+            #     self.customer.append(customer)
+            # self.customer = [customer for customer in self.customer]
+            # else:
+            #     print("This person already have an account")
 
-dict1["Test"] += ["and another string", "one more string"]
 
-print(dict1)
+    # def add_customer(self, customer, account):
+    #     if customer not in self.customer:
+    #         self.customer.update({customer: account})
+    #     else:
+    #         print("Customer already exists")
+    #
+    # def add_account(self, customer, account):
+    #     if customer in self.customer:
+    #         self.customer[customer] += account
+
+
+
 
 
 
 zc_bank = Bank()
 bob = Person(1, "Bob", "Smith")
 bob_checking = Account(2001, "CHECKING", bob)
-zc_bank.add_customer(bob, bob_checking)
+zc_bank.add_customer(bob)
 bob_savings = Account(1001, "SAVINGS", bob)
-zc_bank.add_account(bob, bob_savings)
-# zc_bank.balance_inquiry(1001)
-# # 0
-# zc_bank.deposit(1001, 256.02)
-# zc_bank.balance_inquiry(1001)
-# # 256.02
-# zc_bank.withdrawal(1001, 128)
-# (zc_bank.balance_inquiry(1001))
-# print(zc_bank.balance_inquiry(1001))
-# # 128.02
+zc_bank.add_account(bob)
+zc_bank.balance_inquiry(1001)
+# 0
+zc_bank.deposit(1001, 256.02)
+print(zc_bank.balance_inquiry(1001))
+# 256.02
+zc_bank.withdrawal(1001, 128)
+(zc_bank.balance_inquiry(1001))
+print(zc_bank.balance_inquiry(1001))
+# 128.02
 #
 # wsfs_bank = Bank()
 # imir = Person(7, "Imir", "Ransom")
